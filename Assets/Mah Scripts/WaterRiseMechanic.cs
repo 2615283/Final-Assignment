9ee21ce5a3bcbbf2eab7a75ff6651192f836746c
UvennaR <2438579@students.wits.ac.za>
2023-06-06 17:18:00 +0200

    Flood cards created
    Flood card listing has been created

--- Assets/Mah Scripts/WaterRiseMechanic.cs
+++ Assets/Mah Scripts/WaterRiseMechanic.cs
@@ -7,8 +7,9 @@ public class WaterRiseMechanic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+
+    public List<GameObject> floodcards = new List<GameObject>();
+}
 
     // Update is called once per frame
     void Update()
